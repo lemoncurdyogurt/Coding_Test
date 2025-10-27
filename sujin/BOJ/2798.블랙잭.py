d93902abd4bdf520e499c7685a216b33d3328c9f
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-10-28 01:44:23 +0900

    [DEL] 불필요한 주석코드 삭제

--- sujin/BOJ/2798.블랙잭.py
+++ sujin/BOJ/2798.블랙잭.py
@@ -14,40 +14,4 @@ for i in range(num_N):
                 max_sum = max(max_sum, total)
 
 print(max_sum)
-
-# N_list.sort()
-# print(N_list)
-# #1 2 3 4 5 6 7 8 9 10
-
-# mid_N = len(N_list)//2
-# print(mid_N) #10인 경우 5, 9인경우 4
-
-# random_N_sum = 0
-# for _ in range(3): #10인경우 5,6,7더하기, 9인경우 4,5,6더하기
-#   print(N_list)
-#   random_N_sum += N_list[mid_N]
-#   print(mid_N)
-#   mid_N += 1
-#   print(random_N_sum)
-# print("합",random_N_sum)
-
-# if random_N_sum < M:
-#   mid_N = len(N_list)//2
-#   mid_N += 1
-#   random_N_sum = 0
-#   for _ in range(3): 
-#     random_N_sum += N_list[mid_N]
-#     mid_N += 1
-#   print("새로운합1",random_N_sum)
-# elif random_N_sum > M:
-#   mid_N = len(N_list)//2
-#   mid_N -=1
-#   random_N_sum = 0
-#   for _ in range(3):
-    
-#     random_N_sum += N_list[mid_N]
-#     print(mid_N)
-#     mid_N += 1
-#     print(random_N_sum)
-#   print("새로운합2",random_N_sum)
   
