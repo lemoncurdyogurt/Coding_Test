757d39a0d6822476948990e09f21ac181908cf49
신수진 <80269181+lemoncurdyogurt@users.noreply.github.com>
2026-04-16 17:51:51 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 9472 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString();
+
+const [N, M] = input.split(" ").map(Number);
+// 1부터 N까지의 자연수 중 중복없이 M개 고르기
+
+const visitied = Array(N + 1).fill(false);
+const str = []; // 만들어진 수열의 길이 저장
+
+function backTracking(start, curLength) {
+  if (curLength === M) {
+    console.log(str.join(" "));
+    return;
+  }
+  for (let i = start; i <= N; i++) {
+    if (visitied[i] === false) {
+      visitied[i] = true;
+      str[curLength] = i;
+      backTracking(i + 1, curLength + 1);
+      visitied[i] = false;
+    }
+  }
+}
+
+backTracking(1, 0);
