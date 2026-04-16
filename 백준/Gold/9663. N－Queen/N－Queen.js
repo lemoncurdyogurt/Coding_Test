8904428a9f4d5bbf3ddf5f3dd39a74054c3727ac
신수진 <80269181+lemoncurdyogurt@users.noreply.github.com>
2026-04-16 23:40:23 +0900

    [Gold IV] Title: N-Queen, Time: 3348 ms, Memory: 10744 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.js
+++ 백준/Gold/9663. N－Queen/N－Queen.js
@@ -1,44 +1,57 @@
 const fs = require("fs");
-const input = fs.readFileSync(
-  process.platform === "linux" ? "/dev/stdin" : "input.txt",
-);
+const { debugPort } = require("process");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString();
 
 // 같은 열에 2개 이상 배치X
 // 같은 행에 2개 이상 배치X
 // 같은 대각선 방향에 2개 이상 배치X
 
-N = Number(input);
-grid = Array.from({ length: N }, () => Array(N).fill(0));
+const N = Number(input);
+//const grid = Array.from({ length: N }, () => Array(N).fill(false));
+
+const visitedCol = Array(N).fill(false);
+
+/* (c, r)
+ * (0,0) (0,1) (0,2) (0,3)
+ * (1,0) (1,1) (1,2) (1,3)
+ * (2,0) (2,1) (2,2) (2,3)
+ * (3,0) (3,1) (3,2) (3,3)
+ */
+
+const visitedDiag1 = Array(2 * N).fill(false); // \방향 대각선: c - r + N 값이 동일
+const visitedDiag2 = Array(2 * N).fill(false); // /방향 대각선: c+r 값이 동일
 
 let cnt = 0;
 
-const col = Array(N).fill(false);
-const diag1 = Array(2 * N).fill(false); // row - col + N
-const diag2 = Array(2 * N).fill(false); // row + col
-
-function solve(row) {
-  // 한 행에 모두 퀸을 배치하면 됨
-  if (row === N) {
-    cnt += 1;
+// depth가 col, i가 row
+function backtracking(depth) {
+  if (depth === N) {
+    cnt++;
     return;
   }
 
-  // 현재 행(row)에서 어느 열(col)에 둘 수 있는지 검사
-  for (let c = 0; c < N; c++) {
-    if (col[c] || diag1[row - c + N] || diag2[row + c]) continue;
+  // N줄에 1개씩 배치하는데, 열로 보았을 때 방문 되지 않은 곳만 두기
+  // 대각선도이제 따져봐야함....
+  for (let i = 0; i < N; i++) {
+    if (
+      !visitedCol[i] &&
+      !visitedDiag1[depth - i + N] &&
+      !visitedDiag2[depth + i]
+    ) {
+      visitedCol[i] = true;
+      visitedDiag1[depth - i + N] = true;
+      visitedDiag2[depth + i] = true;
 
-    col[c] = true;
-    diag1[row - c + N] = true;
-    diag2[row + c] = true;
+      backtracking(depth + 1);
 
-    solve(row + 1);
-
-    // 모두 초기화 해줘야함....
-    col[c] = false;
-    diag1[row - c + N] = false;
-    diag2[row + c] = false;
+      visitedCol[i] = false;
+      visitedDiag1[depth - i + N] = false;
+      visitedDiag2[depth + i] = false;
+    }
   }
 }
 
-solve(0);
+backtracking(0);
 console.log(cnt);
