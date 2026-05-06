76248f83cd2cf625da6e38ac278c003e34284c76
신수진 <80269181+lemoncurdyogurt@users.noreply.github.com>
2026-05-06 15:34:54 +0900

    [level 2] Title: 타겟 넘버, Time: 32.13 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,20 +1,4 @@
 function solution(numbers, target) {
     var answer = 0;
-    const N = numbers.length; // 주어진 숫자들의 갯수
-
-    function dfs(index, sum) {
-        if (index === N) {
-            if (sum === target) {
-                answer++;
-            }
-            return;
-        }
-
-        dfs(index + 1, sum + numbers[index]);
-        dfs(index + 1, sum - numbers[index]);
-    }
-    
-    dfs(0,0)
-
     return answer;
 }
